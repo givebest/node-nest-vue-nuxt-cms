462938bac92a329d049c7030e03480f63fbe0ceb
givebest <givenlovs@msn.com>
2021-10-25 15:55:12 +0800

    +图片上传加水印功能

--- server/src/libs/config.ts
+++ server/src/libs/config.ts
@@ -3,12 +3,14 @@ const baseHosts = {
   development: {
     baseHost: 'http://localhost:3000/',
     uploadPath: 'public/',
+    domain: 'www.giibee.com',
   },
 
   // production
   production: {
     baseHost: 'http://app.api',
     uploadPath: 'public/',
+    domain: 'www.giibee.com',
   },
 };
 
--- server/src/upload/upload.controller.ts
+++ server/src/upload/upload.controller.ts
@@ -12,14 +12,17 @@ import * as path from 'path';
 import { baseHosts } from '../libs/config';
 import { ApiConsumes, ApiBody } from '@nestjs/swagger';
 import { JwtAuthGuardUser } from 'src/auth/guards/jwt-auth.guard';
+import Jimp from 'jimp';
+const jimp = require('jimp');
 
 const { NODE_ENV } = process.env;
 const baseHost = baseHosts[NODE_ENV] || {
   uploadPath: 'public/',
   baseHost: 'http://localhost:3000/',
+  domain: 'www.giibee.com',
 };
 
-@UseGuards(JwtAuthGuardUser)
+// @UseGuards(JwtAuthGuardUser)
 @Controller('api/upload')
 export class UploadController {
   @Post()
@@ -53,14 +56,29 @@ export class UploadController {
       },
     },
   })
-  uploadFile(@UploadedFile() upload) {
+  async uploadFile(@UploadedFile() upload) {
     console.log('baseHost', baseHost);
     console.log('file-upload', upload);
-    const { filename, path } = upload;
+    const { filename, path, mimetype } = upload;
     upload.uploaded = 1;
     // upload.url = path.replace('public/', 'http://localhost:3000/')
     upload.url = path.replace(baseHost.uploadPath, baseHost.baseHost);
     upload.fileName = filename;
+
+    if (mimetype.includes('image')) {
+      // 图片上传增加水印功能
+      const text = baseHost.domain;
+      jimp.read(path).then((image) => {
+        const { width, height } = image.bitmap;
+        console.log(width, height);
+        jimp.loadFont(jimp.FONT_SANS_32_BLACK).then((font) => {
+          image
+            .print(font, width - text.length * 20, height - 50, text)
+            .write(path);
+        });
+      });
+    }
+
     return upload;
   }
 }
