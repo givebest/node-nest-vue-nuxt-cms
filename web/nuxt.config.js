8d483784003b0dd9ea3600392270f33623db186b
givebest <givenlovs@msn.com>
2021-10-26 14:14:38 +0800

    +Fix the problem that less-loader version is too high and less cannot be compiled

--- web/nuxt.config.js
+++ web/nuxt.config.js
@@ -19,7 +19,12 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["~/assets/css/base"],
+  css: [
+    {
+      src: "~/assets/css/base.less",
+      lang: "less"
+    }
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ["~/plugins/http"],
